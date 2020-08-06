24923fd5b7a0e58a9b6410a2b53e80cc903629ff
Alex-Coplin <alexander.coplin37@gmail.com>
2020-08-05 19:31:18 -0700

    Create MySQL database

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var database = require('./database/index.js');
 var App = express();
 App.use(express.static(__dirname + '/..' + '/client'));
 App.listen(1215, () => console.log('listening on port 1215'));
