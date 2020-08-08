f714a930a83944dbd9f4b9fc1bef8b790cb6e8b4
Alex-Coplin <alexander.coplin37@gmail.com>
2020-08-08 00:05:48 -0700

    Create get properties request handler

--- server/server.js
+++ server/server.js
@@ -2,8 +2,17 @@ var express = require('express');
 var db = require('./database/index.js');
 var App = express();
 var seeder = require('./database/dbSeeder.js');
-// seeder.reviewSeeder();
-// seeder.propertySeeder();
-// seeder.userSeeder();
+
+App.get('/properties', (req, res) => {
+  db.accessor('SELECT * FROM property')
+    .then((err, data) => {
+      if (err) {
+        res.status(400).send(err);
+      } else {
+        res.send(200, data);
+      }
+    });
+});
+
 App.use(express.static(__dirname + '/..' + '/client'));
 App.listen(1215, () => console.log('listening on port 1215'));
